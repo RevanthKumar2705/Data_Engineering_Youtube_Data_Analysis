49ea6f570e72fa51b35893ec75f38a4d7cd806a1
RevanthKumar2705 <53445876+RevanthKumar2705@users.noreply.github.com>
2025-10-03 13:41:42 -0400

    Update AWs_PYSPARK_code.py

--- AWs_PYSPARK_code.py
+++ AWs_PYSPARK_code.py
@@ -46,12 +46,12 @@ dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ct
 ## @return: datasink4
 ## @inputs: [frame = dropnullfields3]
 
-############################### Added by Darshil ###############################
+############################### ###############################
 #MAKE SURE YOU COPY ONLY WHAT IS NEEDED
 
 datasink1 = dropnullfields3.toDF().coalesce(1)
 df_final_output = DynamicFrame.fromDF(datasink1, glueContext, "df_final_output")
 datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://de-on-youtube-cleansed-useast1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
 
-############################### Added by Darshil ###############################
+#############################################################
 job.commit()
